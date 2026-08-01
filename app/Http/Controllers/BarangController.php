4a5a57ace14cd21fe6bc5ea90e79da420dccfbbc
irfanalfalah75 <irfanalfalah13@gmail.com>
2026-08-01 10:10:48 +0700

    delete dd

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -30,13 +30,6 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-
-    dd([
-        'hasFile' => $request->hasFile('thumbnail'),
-        'isValid' => $request->file('thumbnail')?->isValid(),
-        'disk' => config('filesystems.default'),
-        'public_root' => storage_path('app/public'),
-    ]);
         $thumbnail = null;
 
         if ($request->hasFile('thumbnail')) {
